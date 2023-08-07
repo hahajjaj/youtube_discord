9c24700fd1c0592795b89713c7238631061d4ef8
hahajjaj <hamza96hajjaj@gmail.com>
2023-08-07 09:17:06 +0200

    add stop

--- youtube_to_discord.py
+++ youtube_to_discord.py
@@ -20,6 +20,32 @@ bot = commands.Bot(command_prefix=get_prefix, intents=intents)
 async def on_ready():
     print(f'{bot.user} est connecté!')
     
+queue = []
+
+def check_queue(ctx):
+    if len(queue) > 0:
+        url = queue.pop(0)
+        play_song(ctx, url)
+
+def play_song(ctx, url):
+    voice_client = ctx.voice_client
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': 'temp',  # Nom du fichier temporaire
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        url2 = info['formats'][0]['url']
+        voice_client.play(discord.FFmpegPCMAudio('temp.mp3'), after=lambda e: check_queue(ctx))  # Joue le fichier téléchargé
+
+    
 @bot.command()
 async def stop(ctx):
     voice_client = ctx.voice_client
@@ -30,6 +56,7 @@ async def stop(ctx):
 
     if voice_client.is_playing():
         voice_client.stop()
+        await ctx.send("Musique arrêtée.")
     else:
         await ctx.send("Aucune musique en cours de lecture.")
 
@@ -73,21 +100,24 @@ async def play(ctx, url):
         return
 
     if voice_client.is_playing():
+        queue.append(url)
+        await ctx.send(f'Chanson ajoutée à la file d\'attente.')
+    else:
+        await ctx.send(f'Joue la chanson...')
+        play_song(ctx, url)
+
+@bot.command()
+async def skip(ctx):
+    voice_client = ctx.voice_client
+
+    if voice_client is None:
+        await ctx.send("Je ne suis pas dans un canal vocal.")
+        return
+
+    if voice_client.is_playing():
         voice_client.stop()
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'temp', # Nom du fichier temporaire
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        url2 = info['formats'][0]['url']
-        voice_client.play(discord.FFmpegPCMAudio('temp.mp3')) # Joue le fichier téléchargé
+        await ctx.send("Chanson passée.")
+    else:
+        await ctx.send("Aucune musique en cours de lecture pour sauter.")
 
 bot.run(TOKEN)
